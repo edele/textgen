27d4990ebb3e3f9ece75f21ec3d04accf7107086
AlexSMK <sadomazocat@gmail.com>
2014-03-18 19:56:44 +0400

    next words were done

--- scripts/script.php
+++ scripts/script.php
@@ -4,6 +4,18 @@ function check($text='') {
   return $text;
 }
 
+function nextWords($word, $allWords)
+{
+  $keys = array_keys($allWords, $word);
+  $nextWords = array();
+  for ($i=0; $i < count($keys); $i++) { 
+    $nextKey = $keys[$i];
+    $nextWord =  $allWords[$nextKey+1];
+    array_push($nextWords, $nextWord);
+  }
+  return $nextWords;
+}
+
 function processTxt($path) {
   $txt = file_get_contents($path);
   $allWords = preg_split( "/( |,|\.|;|:|\?|\!|\n)+/", $txt);
@@ -13,12 +25,16 @@ function processTxt($path) {
     $allWords[$i] = mb_strtolower($allWords[$i], "utf8");
   }
 
-  for ($i=0; $i < count($allWords); $i++) { 
+  print_r(nextWords("прекрасно", $allWords));  
+  echo "\n\n";
+
+  for ($i=0; $i < 10000; $i++) { 
     if (!array_key_exists($allWords[$i], $words)) {
-      $words[$allWords[$i]] = 1;
-    } else {
+      $words[$allWords[$i]] = nextWords($allWords[$i], $allWords);
+    } 
+    /*else {
       $words[$allWords[$i]] += 1;
-    }
+    }*/
   }
   print_r($words);
 }
